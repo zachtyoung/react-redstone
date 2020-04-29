1133cc4d25d209e2f0aa1e6e70e5497d3c50b1ec
Zach Young <zachyoung@Zachs-MacBook-Pro.local>
2020-04-28 23:24:39 -0500

    WIP

--- src/components/pages/the-building/the-building.jsx
+++ src/components/pages/the-building/the-building.jsx
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import Breadcrumb from "../../common/breadcrumb/breadcrumb";
 import {Accordion, AccordionItem, AccordionItemHeading, AccordionItemButton, AccordionItemPanel} from 'react-accessible-accordion';
-import {Tab, Tabs, TabList, TabPanel} from 'react-tabs';
+import pdf from './red-pdf.svg'
 import Helmet from "react-helmet";
 import './the-building.scss';
 import CounterBlockStyle from "../../common/countup/countup-style";
@@ -19,25 +19,103 @@ class TheBuildingPage extends Component {
                 <div className="section-wrapper">
                     <div className="container">
                         <div className="text-description text-center">
-                            <div className="row mt-5 pt-5">
+                            <h3>Market Review</h3>
+                            <div className="row mt-5 pt-5 pub-wrap">
                                 <div className="col-lg-6 col-md-12">
-                                <Tabs>
-                            <TabList>
-                             <Tab>2019</Tab>
-                             <Tab>2020</Tab>
-                            </TabList>
-               
-                                <TabPanel>
-                                    <div className='pub-tabs'>
-                                    <div className='pub-tabs-left'>Left</div>
-                                    <div className='pub-tabs-right'>Right</div>
-                                    </div>
-                                </TabPanel>
-                                <TabPanel>
-                       <p>goodbye</p>
-                                </TabPanel>
+                                    <h4>Taxable</h4>
+                                    <Accordion>
+                        <AccordionItem>
+                            <AccordionItemHeading>
+                                <AccordionItemButton>
+                                <span>2020</span>
+                                </AccordionItemButton>
+                            </AccordionItemHeading>
+                            <AccordionItemPanel>
+                                <ul>
+                                <li><img src={pdf}></img>First Quarter of 2019</li>
+                                </ul>
+                            </AccordionItemPanel>
+                        </AccordionItem>
+                        <AccordionItem>
+                            <AccordionItemHeading>
+                                <AccordionItemButton>
+                                    2019
+                                </AccordionItemButton>
+                            </AccordionItemHeading>
+                            <AccordionItemPanel>
+                            <ul>
+                                    <li><img src={pdf}></img>First Quarter of 2019</li>
+                                    <li><img src={pdf}></img>Second Quarter of 2019</li>
+                                    <li><img src={pdf}></img>Third Quarter of 2019</li>
+                                    <li><img src={pdf}></img>Fourth Quarter of 2019</li>
+                                </ul>
+                            </AccordionItemPanel>
+                        </AccordionItem>
+                        <AccordionItem>
+                            <AccordionItemHeading>
+                                <AccordionItemButton>
+                                   2018
+                                </AccordionItemButton>
+                            </AccordionItemHeading>
+                            <AccordionItemPanel>
+                            <ul>
+                                    <li><img src={pdf}></img>First Quarter of 2018</li>
+                                    <li><img src={pdf}></img>Second Quarter of 2018</li>
+                                    <li><img src={pdf}></img>Third Quarter of 2018</li>
+                                    <li><img src={pdf}></img>Fourth Quarter of 2018</li>
+                                </ul>
+                            </AccordionItemPanel>
+                        </AccordionItem>
+                    </Accordion>
                         
-                        </Tabs>
+                                </div>
+                                <div className="col-lg-6 col-md-12">
+                                    <h4>Municipal</h4>
+                                    <Accordion>
+                        <AccordionItem>
+                            <AccordionItemHeading>
+                                <AccordionItemButton>
+                                    2020
+                                </AccordionItemButton>
+                            </AccordionItemHeading>
+                            <AccordionItemPanel>
+                            <ul>
+                                <li><img src={pdf}></img>First Quarter of 2019</li>
+                                </ul>
+                            </AccordionItemPanel>
+                        </AccordionItem>
+                        <AccordionItem>
+                            <AccordionItemHeading>
+                                <AccordionItemButton>
+                                   2019
+                                </AccordionItemButton>
+                            </AccordionItemHeading>
+                            <AccordionItemPanel>
+                            <ul>
+                                    <li><img src={pdf}></img>First Quarter of 2019</li>
+                                    <li><img src={pdf}></img>Second Quarter of 2019</li>
+                                    <li><img src={pdf}></img>Third Quarter of 2019</li>
+                                    <li><img src={pdf}></img>Fourth Quarter of 2019</li>
+                                </ul>
+                            </AccordionItemPanel>
+                        </AccordionItem>
+                        <AccordionItem>
+                            <AccordionItemHeading>
+                                <AccordionItemButton>
+                                    2018
+                                </AccordionItemButton>
+                            </AccordionItemHeading>
+                            <AccordionItemPanel>
+                            <ul>
+                                    <li><img src={pdf}></img>First Quarter of 2018</li>
+                                    <li><img src={pdf}></img>Second Quarter of 2018</li>
+                                    <li><img src={pdf}></img>Third Quarter of 2018</li>
+                                    <li><img src={pdf}></img>Fourth Quarter of 2018</li>
+                                </ul>
+                            </AccordionItemPanel>
+                        </AccordionItem>
+                    </Accordion>
+                        
                                 </div>
                             </div>
                         </div>
